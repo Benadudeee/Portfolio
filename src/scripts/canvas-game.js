df66f81a8e56b3cbe601e2523fbf4bfd623ed793
Benedict A <sbenedict637@gmail.com>
2025-04-22 14:50:05 -0400

    Made shape rotate from center

--- src/scripts/canvas-game.js
+++ src/scripts/canvas-game.js
@@ -7,4 +7,44 @@ console.log(dimensions.width, dimensions.height, dimensions["0"]);
 canvas.width = dimensions.width;
 canvas.height = dimensions.height;
 
-ctx.fillRect(100, 100, 50, 50);
+
+
+let rot = 0
+;
+function draw(){
+    ctx.save();
+    // let rotateConst = Math.PI / (180 * rot);
+    //Rotate on center
+    ctx.translate(75, 75);
+    ctx.rotate(rot);
+
+    ctx.fillRect(-25,-25, 50, 50);
+    ctx.restore();
+
+}
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    draw();
+    rot += 0.01;
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+// const img = new Image();
+// img.src = "data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw==";
+
+// ctx.drawImage(img, 0, 0, );
+
+// const images = [
+//     {src:"",},
+//     {}
+// ];
+
+// Image Loading function ()
+// await Promise.all(
+//     Array.from(document.images).map(
+//       (image) =>
+//         new Promise((resolve) => image.addEventListener("load", resolve)),
+//     ),
+//   );
