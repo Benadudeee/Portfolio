837033919d7ee059e3dc18febf45126e54d384d4
Benedict A <sbenedict637@gmail.com>
2025-05-09 08:29:41 -0400

    Added click events and spawn cycle for objects

--- src/scripts/canvas-game.js
+++ src/scripts/canvas-game.js
@@ -1,46 +1,115 @@
+import { chunkToString } from "astro/runtime/server/render/common.js";
+
 const canvas = document.querySelector(".hero--panel");
 const ctx = canvas.getContext("2d");
 
 const dimensions = canvas.getClientRects()["0"];
-console.log(dimensions.width, dimensions.height, dimensions["0"]);
+console.log(dimensions.width);
 
 canvas.width = dimensions.width;
 canvas.height = dimensions.height;
 
+class Rect{
+    constructor(x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.rotation = 0;
+
+        this.isDead = false;
+    }
+    draw(){
+        const translateX = (this.x + this.width) / 2;
+        const translateY = (this.y + this.height) / 2;
+        
+        ctx.save();
+        
+        ctx.translate(translateX, translateY);
+        ctx.rotate(this.rotation);
+        
+        ctx.fillRect(-this.width/2, -this.height/2, this.width, this.height );
+        ctx.restore();
+    }
+    
+    update(){
+        const rotationSpeed = 0.00125;
+        
+        this.rotation += rotationSpeed;
+        this.y += 10;
+    }
+
+    render(){
+        this.draw();
+        this.update();
+    }
+}
+
+addEventListener("click", (e) => {
+    const x = e.clientX - canvas.getBoundingClientRect().left;
+    const y = e.clientY - canvas.getBoundingClientRect().top;
+
+    // console.log(x, y, objs);
+
+    console.log(x, y);
+
+    objs.forEach((obj) => {
+        // Since the object transform alters the x and y coordinates, we need to take that into account.
+        // This is achieved by diviging the x and y coordinates by 2, since the object is drawn at half of its width and height.
+
+        if (x > obj.x / 2 && x < (obj.x + obj.width / 2) && 
+            y > obj.y / 2 && y < (obj.y + obj.height / 2)) {
+                obj.isDead = true;
+        }
+    })
+})
+
+const objs = [];
 
 
-let rot = 0
-;
-function draw(){
-    ctx.save();
-    // let rotateConst = Math.PI / (180 * rot);
-    //Rotate on center
-    ctx.translate(75, 75);
-    ctx.rotate(rot);
+function drawLines(){
+    let x = 100;
 
-    ctx.fillRect(-25,-25, 50, 50);
-    ctx.restore();
+    while(x < canvas.width){
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+
+        x += 100;
+        console.log(x);
+    }
 
 }
+
+// Testing
+objs.push(new Rect(100, 100, 100, 100));
+
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    draw();
-    rot += 0.01;
+
+    objs.filter((obj) => !obj.isDead).forEach((obj) => {
+
+        obj.render();
+    })
     requestAnimationFrame(animate);
 }
 
+let intervalTime = 2000;
+window.setInterval(() => {
+    const min = 100;
+    const size = 100;
+    const max = (Math.round( canvas.width / 100 ) * 200) - 400; // For some reason, rects are placed on half of the max bound (Given when it's divisible by 100) 
+    
+    const y = -size - 100;
+    const x = Math.random() * (max - min) + min; // Random number between 100 and max
+    objs.push(new Rect(x, y, size, size));
+
+}, intervalTime)
+
 animate();
-
-// const img = new Image();
-// img.src = "data:image/gif;base64,R0lGODlhCwALAIAAAAAA3pn/ZiH5BAEAAAEALAAAAAALAAsAAAIUhA+hkcuO4lmNVindo7qyrIXiGBYAOw==";
-
-// ctx.drawImage(img, 0, 0, );
-
-// const images = [
-//     {src:"",},
-//     {}
-// ];
-
 // Image Loading function ()
 // await Promise.all(
 //     Array.from(document.images).map(
